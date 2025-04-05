970a06c8e13332de8326048d432c2e3f504e22a0
litingyes <luz.liting@gmail.com>
2025-04-06 00:01:51 +0800

    feat(home): add ast card

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,34 @@
 </script>
 
 <template>
-  <div class="flex items-center justify-center h-full w-full">
-    <div class="font-bold text-2xl">
-      TransVisor
+  <div class="flex items-center justify-center h-full w-full overflow-auto">
+    <div class="-translate-y-20">
+      <InspiraBlurReveal :duration="0.6" :delay="0.6" :y-offset="60">
+        <div class="font-bold text-6xl bg-gradient-to-r from-primary-700 to-primary-300 bg-clip-text  text-transparent">
+          TransVisor
+        </div>
+        <p class="text-neutral-500 text-2xl">
+          Focus on data conversion and visualization
+        </p>
+        <div class="mt-6">
+          <div
+            class="relative p-4 rounded-lg border border-neutral-200 cursor-pointer transition-all hover:bg-neutral-50"
+            @click="navigateTo('/ast/markdown')"
+          >
+            <InspiraGlowingEffect
+              :spread="40"
+              :glow="true"
+              :disabled="false"
+              :proximity="64"
+              :inactive-zone="0.01"
+            />
+            <div class="flex items-center gap-2">
+              <UIcon name="i-lucide:tree-deciduous" />
+              AST Explorer
+            </div>
+          </div>
+        </div>
+      </InspiraBlurReveal>
     </div>
   </div>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
+import { setupInspiraUI } from '@inspira-ui/plugins'
+import animate from 'tailwindcss-animate'
 
 export default <Partial<Config>>{
-
+  plugins: [animate, setupInspiraUI],
 }
