90738de667cc48d8277e5e912999b761cc4c39ef
litingyes <luz.liting@gmail.com>
2025-04-05 15:39:07 +0800

    fix(ast): watch options change

--- pages/ast/[[language]]/index.vue
+++ pages/ast/[[language]]/index.vue
@@ -1,19 +1,18 @@
 <script lang="ts" setup>
 const { language, hashData, updateHash } = useUrlInfo()
 
-const code = ref(hashData.value.code)
-
 const { parse } = useAst()
 
 const tc = ref(0)
 const ast = ref()
-watch(code, (code) => {
-  parse(code ?? '').then(({ timeConsuming, ast: data }) => {
+watch(hashData, (data) => {
+  parse(data.code ?? '').then(({ timeConsuming, ast: data }) => {
     tc.value = timeConsuming
     ast.value = data
   })
 }, {
   immediate: true,
+  deep: true,
 })
 
 function onCodeChange(value: string) {
@@ -52,7 +51,7 @@ function onCodeChange(value: string) {
     </header>
     <main class="h-[calc(100%-3rem)] grid grid-cols-2">
       <div class="h-full">
-        <MonacoEditor v-model="code" @change="onCodeChange" />
+        <MonacoEditor :model-value="hashData.code" @change="onCodeChange" />
       </div>
       <div class="px-4 py-2 overflow-auto">
         <AstTree :value="ast" />
